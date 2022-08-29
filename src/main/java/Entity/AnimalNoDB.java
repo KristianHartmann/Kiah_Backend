d4ac68bf156069b4c3b2a4246ed1957e369ab4a1
KristianHartmann <krha998@yahoo.dk>
2022-08-29 14:02:24 +0200

    Initial commit

---
+++ src/main/java/Entity/AnimalNoDB.java
@@ -0,0 +1,17 @@
+package Entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AnimalNoDB {
+    String type;
+    String sound;
+
+    public AnimalNoDB(String type, String sound) {
+        this.type = type;
+        this.sound = sound;
+    }
+}
---
+++ src/main/java/kiah/services/kiah/AnimalDemo.java
@@ -0,0 +1,40 @@
+package kiah.services.kiah;
+
+import Entity.AnimalNoDB;
+import com.google.gson.Gson;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("/animal")
+public class AnimalDemo {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Vuf... (Message from a dog)";
+    }
+
+
+    @GET
+    @Path("/animal_list")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getList() {
+        return "[\"Dog\", \"Cat\", \"Mouse\", \"Bird\"]";
+    }
+
+    @GET
+    @Path("/animal")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getObject() {
+        AnimalNoDB animalNoDB = new AnimalNoDB("Dog", "Vuf");
+        return new Gson().toJson(animalNoDB);
+    }
+
+
+}
